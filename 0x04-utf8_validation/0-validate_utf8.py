28f0d8fdb609a8058fcf370fdda90004b96673c8
vanessa okumu <okumuvanessa@gmail.com>
2024-11-16 10:02:03 +0000

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python3
+"""
+Module to determine if a given data set represents a valid UTF-8 encoding.
+"""
 
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
     
     Args:
-        data (list): A list of integers where each integer represents a byte of data.
-    
+        data (list of int): A list of integers where each integer represents 1 byte of data.
+
     Returns:
-        bool: True if the data represents a valid UTF-8 encoding, False otherwise.
+        bool: True if data is a valid UTF-8 encoding, else False.
     """
-    # Check for each byte in the data
     num_bytes = 0
 
     for byte in data:
-        # Ensure the byte is between 0 and 255
-        if byte < 0 or byte > 255:
-            return False
+        # Get the least significant 8 bits of the integer
+        byte = byte & 0xFF
         
-        # If we're at the beginning of a character sequence
         if num_bytes == 0:
-            if (byte >> 7) == 0b0:  # 1-byte character (0xxxxxxx)
-                num_bytes = 0
-            elif (byte >> 5) == 0b110:  # 2-byte character (110xxxxx)
+            # Determine the number of bytes in the UTF-8 character
+            if (byte >> 5) == 0b110:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:  # 3-byte character (1110xxxx)
+            elif (byte >> 4) == 0b1110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 4-byte character (11110xxx)
+            elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            else:
-                return False  # Invalid starting byte for UTF-8
+            elif (byte >> 7):
+                return False
         else:
-            # Continuation byte (10xxxxxx)
+            # Check that the byte is a continuation byte
             if (byte >> 6) != 0b10:
                 return False
-            num_bytes -= 1
+        num_bytes -= 1
 
-    # If we've finished processing and still have bytes left to process, it's invalid
     return num_bytes == 0
