e0b383e46c8b1a6d6843294b8b6e1edb4c699f4d
vanessa okumu <okumuvanessa@gmail.com>
2024-11-16 09:24:14 +0000

    Pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-0-main
+Module to generate Pascal's triangle.
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    """
     if n <= 0:
         return []
     
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
     
     return triangle
