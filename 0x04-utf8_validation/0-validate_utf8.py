2d599d26ca20d2af41a7c76526a2233447fb7a45
vanessa okumu <okumuvanessa@gmail.com>
2024-11-16 09:53:10 +0000

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,45 +1,36 @@
 #!/usr/bin/python3
-"""
-Module to determine if a given data set represents a valid UTF-8 encoding.
-"""
 
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
     
     Args:
-        data (list of int): A list of integers where each integer represents 1 byte of data.
-
+        data (list): A list of integers where each integer represents a byte of data.
+    
     Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
+        bool: True if the data represents a valid UTF-8 encoding, False otherwise.
     """
+    # Number of bytes to be checked in a multi-byte sequence
     num_bytes = 0
 
     for byte in data:
-        # Get the least significant 8 bits of the integer
-        byte = byte & 0xFF
-        
+        # Check for the number of leading 1's in the byte
         if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (byte >> 5) == 0b110:
+            if (byte >> 7) == 0b0:  # 1-byte character
+                num_bytes = 0
+            elif (byte >> 5) == 0b110:  # 2-byte character
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:
+            elif (byte >> 4) == 0b1110:  # 3-byte character
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:
+            elif (byte >> 3) == 0b11110:  # 4-byte character
                 num_bytes = 3
-            elif (byte >> 7):
-                return False
+            else:
+                return False  # Invalid starting byte for UTF-8
         else:
-            # Check that the byte is a continuation byte
+            # Check if it is a continuation byte (should start with 10xxxxxx)
             if (byte >> 6) != 0b10:
                 return False
-        num_bytes -= 1
-
+            num_bytes -= 1
+    
+    # If we finish and there's still a byte expected, the data is invalid
     return num_bytes == 0
-
-# Ensure this file is being run directly before executing the function
-if __name__ == "__main__":
-    import sys
-    # Example usage
-    data = [int(i) for i in sys.argv[1:]]
-    print(validUTF8(data))
